692296e6623f68f9a0e4de825b8d893ee9f35938
Sookim18 <scstnghks@naver.com>
2021-06-15 21:55:05 +0900

    feat : DetailViewController에서 Alert 추가

--- CloudNotes/CloudNotes/ViewController/DetailViewController.swift
+++ CloudNotes/CloudNotes/ViewController/DetailViewController.swift
@@ -16,6 +16,8 @@ class DetailViewController: UIViewController, SendDataDelegate {
         return view
     }()
     
+    var currentIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +27,8 @@ class DetailViewController: UIViewController, SendDataDelegate {
     }
     
     @objc func editMemo() {
+        guard let editButton = self.navigationItem.rightBarButtonItem else { return }
+        detailActionSheet(editButton)
     }
     
     private func setTextViewConstraint() {
@@ -36,6 +40,59 @@ class DetailViewController: UIViewController, SendDataDelegate {
     
     func sendData(data: Memo, index: Int) {
         self.memoDetailTextView.text = "\(data.title!) + \(data.body!) + \(index)"
+    }
+    
+}
+
+extension DetailViewController {
+    
+    func deleteAlert() {
+        let defaultAction = UIAlertAction(title: "삭제", style: .destructive) { (action) in
+            DataManager.shared.deleteData(index: self.currentIndex)
+            NotificationCenter.default.post(name: NSNotification.Name("didRecieveNotification"), object: nil, userInfo: nil )
+            if self.splitViewController?.traitCollection.horizontalSizeClass == .compact {
+                self.splitViewController?.show(.primary)
+            }
+            else {
+                self.sendData(data: DataManager.shared.memoList[self.currentIndex], index: self.currentIndex)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        
+        let alert = UIAlertController(title: "진짜요?", message: "정말로 삭제하시겠어요?", preferredStyle: .alert)
+        alert.addAction(defaultAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true)
+    }
+    
+    func detailActionSheet(_ sender: UIBarButtonItem) {
+        let destroyAction = UIAlertAction(title: "Delete", style: .destructive) { (action) in
+            self.deleteAlert()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let shareAction = UIAlertAction(title: "Share..", style: .default) { (action) in
+            self.presentShareSheet(sender)
+        }
+             
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(shareAction)
+        alert.addAction(destroyAction)
+        alert.addAction(cancelAction)
+        
+        if let popOverController = alert.popoverPresentationController { popOverController.barButtonItem = sender }
+        
+        self.present(alert, animated: true)
+    }
+    
+    func presentShareSheet(_ sender: UIBarButtonItem) {
+        guard let sendText = self.memoDetailTextView.text else { return }
+        
+        let shareSheetViewController = UIActivityViewController(activityItems: [sendText], applicationActivities: nil)
+    
+        if let popOverController = shareSheetViewController.popoverPresentationController { popOverController.barButtonItem = sender }
+        
+        present(shareSheetViewController, animated: true)
     }
     
 }
